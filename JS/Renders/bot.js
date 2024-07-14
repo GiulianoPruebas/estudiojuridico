e0f40dd58e3fac8edbb3ec579258a0dbdf614aee
GiulianoMendoza <giulianovictorm98@gmail.com>
2024-07-14 00:05:42 -0300

    asd7

---
+++ JS/Renders/bot.js
@@ -0,0 +1,377 @@
+const preguntas = [
+    {
+        pregunta: "¿Sufriste un accidente?",
+        subpreguntas: [
+            {
+                pregunta: "¿En el trabajo?",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Estás de ART?",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Iniciaron actuaciones en dependencia para encuadrar las lesiones en y por acto del servicio?",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te notificaron de la resolución?"
+                                    },
+                                    {
+                                        pregunta: "No me notificaron de la resolución"
+                                    }
+                                ]
+                            },
+                            {
+                                pregunta: "¿No te notificaron del inicio de actuaciones?",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te notificaron de la resolución?"
+                                    },
+                                    {
+                                        pregunta: "No me notificaron de la resolución"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "¿La ART te rechazó el siniestro o te dio el alta?",
+                        subpreguntas: [
+                            {
+                                pregunta: "Pasaron 2 años desde el alta o rechazo de la ART?"
+                            },
+                            {
+                                pregunta: "No pasaron 2 años desde el alta o rechazo"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                pregunta: "¿Mientras ibas o venías del trabajo?",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Estás de ART?",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Iniciaron actuaciones en dependencia para encuadrar las lesiones en y por acto del servicio?",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te notificaron de la resolución?"
+                                    },
+                                    {
+                                        pregunta: "No me notificaron de la resolución"
+                                    }
+                                ]
+                            },
+                            {
+                                pregunta: "¿No te notificaron del inicio de actuaciones?",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te notificaron de la resolución?"
+                                    },
+                                    {
+                                        pregunta: "No me notificaron de la resolución"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "¿La ART te rechazó el siniestro o te dio el alta?",
+                        subpreguntas: [
+                            {
+                                pregunta: "Pasaron 2 años desde el alta o rechazo de la ART?"
+                            },
+                            {
+                                pregunta: "No pasaron 2 años desde el alta o rechazo"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        pregunta: "¿Tuviste un enfretamiento y sufriste lesiones?",
+        subpreguntas: [
+            {
+                pregunta: "¿Estás de ART?",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Iniciaron actuaciones en dependencia para encuadrar las lesiones en y por acto del servicio?",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Te notificaron de la resolución?"
+                            },
+                            {
+                                pregunta: "No me notificaron de la resolución"
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "¿No te notificaron del inicio de actuaciones?",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Te notificaron de la resolución?"
+                            },
+                            {
+                                pregunta: "No me notificaron de la resolución"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                pregunta: "¿La ART te rechazó el siniestro o te dio el alta?",
+                subpreguntas: [
+                    {
+                        pregunta: "Pasaron 2 años desde el alta o rechazo de la ART?"
+                    },
+                    {
+                        pregunta: "No pasaron 2 años desde el alta o rechazo"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        pregunta: "¿Tenés un SA en Trámite?",
+        subpreguntas: [
+            {
+                pregunta: "En la OTC (Oficina de transparencia)",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Te noficiaron del inicio del SA?"
+                    }, {
+
+                        pregunta: "¿Declaraste en el SA?"
+                    },
+                    {
+                        pregunta: "¿Te Notificaron de una sanción?"
+                    }
+                ]
+            },
+            {
+                pregunta: "En Desempeño profesional",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Te noficiaron del inicio del SA?"
+                    }, {
+
+                        pregunta: "¿Declaraste en el SA?"
+                    },
+                    {
+                        pregunta: "¿Te Notificaron de una sanción?"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        pregunta: "¿Te pasaron a disponibilidad o pasiva?",
+        subpreguntas: [
+            {
+                pregunta: "¿Estuviste de ART por este motivo?",
+                subpreguntas: [
+                    {
+                        pregunta: "¿Iniciaron actuaciones en dependencia para encuadrar El evento en y por acto del servicio?",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Te notificaron de la resolución?"
+                            },
+                            {
+                                pregunta: "No me notificaron de la resolución"
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "No iniciaron actuaciones de encuadre"
+                    }
+                ]
+            },
+            {
+                pregunta: "¿Tenes un sumario por este motivo?",
+                subpreguntas: [
+                    {
+                        pregunta: "En la OTC (Oficina de transparencia)",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Te noficiaron del inicio del SA?"
+                            }, {
+        
+                                pregunta: "¿Declaraste en el SA?"
+                            },
+                            {
+                                pregunta: "¿Te Notificaron de una sanción?"
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "En Desempeño profesional",
+                        subpreguntas: [
+                            {
+                                pregunta: "¿Te noficiaron del inicio del SA?"
+                            }, {
+        
+                                pregunta: "¿Declaraste en el SA?"
+                            },
+                            {
+                                pregunta: "¿Te Notificaron de una sanción?"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                pregunta: "¿Te aplicaron el protocolo de violencia de genero?",
+                subpreguntas: [
+                    {
+                        pregunta: "Te notificaron del inicio de SA por este motivo?",
+                        subpreguntas: [
+                            {
+                                pregunta: "En la OTC (Oficina de transparencia)",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te noficiaron del inicio del SA?"
+                                    }, {
+                
+                                        pregunta: "¿Declaraste en el SA?"
+                                    },
+                                    {
+                                        pregunta: "¿Te Notificaron de una sanción?"
+                                    }
+                                ]
+                            },
+                            {
+                                pregunta: "En Desempeño profesional",
+                                subpreguntas: [
+                                    {
+                                        pregunta: "¿Te noficiaron del inicio del SA?"
+                                    }, {
+                
+                                        pregunta: "¿Declaraste en el SA?"
+                                    },
+                                    {
+                                        pregunta: "¿Te Notificaron de una sanción?"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        pregunta: "No te notificaron del inicio de SA por este motivo"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        pregunta: "¿Te notificaron de una resolución?",
+        subpreguntas: [
+            {
+                pregunta: "Pasaron más de 15 días habiles"
+            },
+            {
+                pregunta: "No pasaron más de 15 días habiles"
+            }
+        ]
+    }
+];
+function generarPreguntasIniciales() {
+    const chatbotContainer = document.getElementById('chatbot-container');
+    chatbotContainer.innerHTML = ''; 
+
+    preguntas.forEach((pregunta, index) => {
+        const preguntaDiv = document.createElement('div');
+        preguntaDiv.classList.add('message', 'bot-message', 'pregunta');
+        preguntaDiv.textContent = pregunta.pregunta;
+        preguntaDiv.onclick = () => mostrarSubpreguntas(pregunta, index);
+
+        chatbotContainer.appendChild(preguntaDiv);
+    });
+}
+function mostrarSubpreguntas(pregunta, index) {
+    const chatbotContainer = document.getElementById('chatbot-container');
+    chatbotContainer.innerHTML = ''; 
+    const userMessageDiv = document.createElement('div');
+    userMessageDiv.classList.add('message', 'user-message');
+    userMessageDiv.textContent = pregunta.pregunta;
+    chatbotContainer.appendChild(userMessageDiv);
+    pregunta.subpreguntas.forEach((subpregunta, subindex) => {
+        const subpreguntaDiv = document.createElement('div');
+        subpreguntaDiv.classList.add('message', 'bot-message', 'subpregunta');
+        subpreguntaDiv.textContent = subpregunta.pregunta; 
+        subpreguntaDiv.onclick = () => mostrarRespuestas(subpregunta, index, subindex);
+
+        chatbotContainer.appendChild(subpreguntaDiv);
+    });
+    const optionDiv = document.createElement('div');
+    optionDiv.classList.add('message', 'bot-message', 'option');
+    optionDiv.textContent = "Menú";
+    optionDiv.onclick = generarPreguntasIniciales;
+
+    chatbotContainer.appendChild(optionDiv);
+}
+function mostrarRespuestas(subpregunta, index, subindex) {
+    const chatbotContainer = document.getElementById('chatbot-container');
+    chatbotContainer.innerHTML = ''; 
+    const userMessageDiv = document.createElement('div');
+    userMessageDiv.classList.add('message', 'user-message');
+    userMessageDiv.textContent = subpregunta.pregunta; 
+    chatbotContainer.appendChild(userMessageDiv);
+    if (subpregunta.subpreguntas && subpregunta.subpreguntas.length > 0) {
+        subpregunta.subpreguntas.forEach((respuesta, subsubindex) => {
+            const respuestaDiv = document.createElement('div');
+            respuestaDiv.classList.add('message', 'bot-message', 'respuesta');
+            respuestaDiv.textContent = respuesta.pregunta; 
+            respuestaDiv.onclick = () => mostrarRespuestas(respuesta, index, subindex, subsubindex);
+            chatbotContainer.appendChild(respuestaDiv);
+        });
+    } else {
+        const noRespuestaDiv = document.createElement('div');
+        noRespuestaDiv.classList.add('message', 'bot-message', 'respuesta');
+        noRespuestaDiv.innerHTML = 'Complete el siguiente formulario y nos contactaremos a la brevedad.<br><a href="formulario.html" target="_blank" class="form-a">Click aquí</a>';
+        chatbotContainer.appendChild(noRespuestaDiv);
+    }
+    const optionDiv = document.createElement('div');
+    optionDiv.classList.add('message', 'bot-message', 'option');
+    optionDiv.textContent = "SubMenú";
+    optionDiv.onclick = () => mostrarSubpreguntas(preguntas[index], index);
+
+    chatbotContainer.appendChild(optionDiv);
+}
+generarPreguntasIniciales();
+const modal = document.getElementById("chatbotModal");
+const btn = document.getElementById("openChatbot");
+const span = document.getElementById("closeChatbot");
+const robotImage = document.getElementById("robotImage");
+function switchToRobotImage() {
+    robotImage.style.display = "block";
+}
+
+btn.onclick = function() {
+    modal.style.right = "90px";
+    modal.style.display = "block";
+    switchToRobotImage();
+}
+
+span.onclick = function() {
+    modal.style.right = "-320px"; 
+    setTimeout(() => {
+        generarPreguntasIniciales();
+        modal.style.display = "none";
+        switchToRobotImage();
+    }, 300); 
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.right = "-320px"; 
+        setTimeout(() => {
+            modal.style.display = "none";
+            switchToRobotImage();
+        }, 300); 
+    }
+}
+setTimeout(() => {
+    switchToRobotImage(); // Simula respuesta del chatbot
+}, 4000);
