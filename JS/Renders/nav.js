0a647d9e7a00bd3bcef20e9c8892ee4c9d08b964
GiulianoMendoza <giulianovictorm98@gmail.com>
2024-08-24 10:06:37 -0300

    corecciones

--- JS/Renders/nav.js
+++ JS/Renders/nav.js
@@ -31,10 +31,12 @@ function GenerarNav() {
         <input type="checkbox" id="toggle">
         <label for="toggle">&#9776;</label>
         <ul class="menu-dropdown">
-            <li><a href="#">Sobre Nosotros</a></li>
-            <li><a href="#">Retiros</a></li>
-            <li><a href="#">Defensas Disciplinarias</a></li>
-            <li><a href="#">Derechos Laborales</a></li>
+            <li><a href="Nosotros.html">Sobre Nosotros</a></li>
+            <li><a href="Retiros.html">Lesiones y Retiros</a></li>
+            <li><a href="Reclamos.html">Reclamos Administrativo</a></li>
+            <li><a href="Accidentes.html">Accidentes</a></li>
+            <li><a href="Sumarios.html">Sanciones o Sumarios</a></li>
+            <li><a href="Junta.html">Junta Medica/Calificaciones</a></li>
         </ul>
     </div>
     `;
@@ -43,7 +45,7 @@ function GenerarNav() {
 const contenedor = document.getElementById('navPrincipal');
 const barraNavegacion = GenerarNav();
 contenedor.appendChild(barraNavegacion);
-document.getElementById('toggle').addEventListener('change', function() {
+document.getElementById('toggle').addEventListener('change', function () {
     var menuDropdown = document.querySelector('.menu-dropdown');
     if (this.checked) {
         menuDropdown.style.display = 'block';
